48388aaebe69188c0bb0f3a2c037148cd5ef6e2f
Dwight Spencer <dspencer@computekindustries.com>
2017-09-05 21:46:42 -0500

    Update __init__.py

--- authz/__init__.py
+++ authz/__init__.py
@@ -3,36 +3,11 @@
 from .version import __version__
 from .version import __author__
 from .version import __licence__
+from .exceptions import AuthSuccess, AuthFailure
 
 import base64, json
 
 from flask import Flask, jsonify, request
-
-class AuthSuccess(Exception):
-   def __init__(self, message=""):
-        
-        if not message:
-            message = "The request authorization succeeded."
-
-        # Call the base class constructor with the parameters it needs
-        super(AuthSuccess, self).__init__(message)
-        
-        self.allow = True
-        
-class AuthFailure(Exception):
-   def __init__(self, message="", error=""):
-    
-        if not message:
-            message = "The request authorization failed. You must say hello first"
-        
-        if not error:
-            error = "You must say hello first."
-        
-        # Call the base class constructor with the parameters it needs
-        super(AuthFailure, self).__init__(message)
-        
-        self.allow = False
-        self.error = error
 
 class DockerAuthApp(object):
 
