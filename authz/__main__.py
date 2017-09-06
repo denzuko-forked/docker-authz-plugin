4ff1eced9c7966c73335f8e638f7229ce1bf183c
Dwight Spencer <dspencer@computekindustries.com>
2017-09-05 21:44:22 -0500

    Update __main__.py

--- authz/__main__.py
+++ authz/__main__.py
@@ -1,4 +1,9 @@
 # -~- coding: utf-8 -~-
+
+from .version import __version__
+from .version import __author__
+from .version import __licence__
+
 from . import DockerAuthApp
 
 if __name__ == "__main__":
