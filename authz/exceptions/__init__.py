2aee20aca2ea34b4c3534b515ae33f99d34d4697
Dwight Spencer <dspencer@computekindustries.com>
2017-09-05 21:48:37 -0500

    Create __init__.py

---
+++ authz/exceptions/__init__.py
@@ -0,0 +1,31 @@
+# -~- coding: utf-8 -~-
+
+from authz.version import __version__
+from authz.version import __author__
+from authz.version import __licence__
+
+class AuthSuccess(Exception):
+   def __init__(self, message=""):
+        
+        if not message:
+            message = "The request authorization succeeded."
+
+        # Call the base class constructor with the parameters it needs
+        super(AuthSuccess, self).__init__(message)
+        
+        self.allow = True
+        
+class AuthFailure(Exception):
+   def __init__(self, message="", error=""):
+    
+        if not message:
+            message = "The request authorization failed. You must say hello first"
+        
+        if not error:
+            error = "You must say hello first."
+        
+        # Call the base class constructor with the parameters it needs
+        super(AuthFailure, self).__init__(message)
+        
+        self.allow = False
+        self.error = error
