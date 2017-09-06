2f7db3c743aa69f67d966d3981b55fcfd377caa9
Dwight Spencer <dspencer@computekindustries.com>
2017-09-05 21:41:59 -0500

    Create version.py

---
+++ authz/version.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+"""
+    Docker Authz Plugin
+    ~~~~~~~~
+    Authentication Example for Docker REST API.
+    :copyright: Copyright ©2017, Dwight Spencer. Some Rights Reserved.
+    :license: MIT License, see LICENSE for more details.
+    :authors: Dwight Spencer <support@dapla.net>
+"""
+
+__pkgname__ = "docker-authz-plugin"
+__author__ = "Dwight Spencer"
+__author_email__ = "support@dapla.net"
+__versioninfo__ = (0, 0, 1)
+__version__ = '.'.join(map(str, __versioninfo__))
